7e2abc99ac8593b359a312783e4ac785689b1aca
Bartek <barltomiej.nicewicz1993@gmail.com>
2026-07-02 23:22:07 +0200

    first commit

--- siec_przemyslowa_projekt/siec_przemyslowa/siec_przemyslowa_app_v1/models.py
+++ siec_przemyslowa_projekt/siec_przemyslowa/siec_przemyslowa_app_v1/models.py
@@ -6,9 +6,12 @@ from . modele.models_line_productions import *
 from . modele.modele_status_urzadzenia import *
 from . managers.managers_linia_produkcyjna import *
 from . managers.managers_urzadzenia_maszyny import *
+from datetime import datetime
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
 class LiniaProdukcyjna(models.Model):
     Nazwa_linii=models.CharField(max_length=255,unique=True,blank=False,null=False)
-    Strefa_linia_produkcyjna=models.IntegerField(choices=LiniaProdukcyjnaStrefa.choices,unique=True,blank=False,null=False)
+    Strefa_linia_produkcyjna=models.IntegerField(choices=LiniaProdukcyjnaStrefa.choices,unique=False,blank=False,null=False)
     Opis=models.CharField(null=False,blank=True,max_length=1000,default='')
 
     objects=models.Manager()
@@ -21,6 +24,16 @@ class LiniaProdukcyjna(models.Model):
         verbose_name_plural='Linie produkcyjne'
         ordering=['Nazwa_linii']
 
+# @receiver([pre_save],sender=LiniaProdukcyjna)
+# def zapisz_linie_produkcyjna_przed(sender,instance,**kwargs):
+#     print("Właśnie mamy zapisać: ")
+#     nasza_linia_produkcyjna=LiniaProdukcyjna.objects.get(id=instance.id)
+#     print(f"{nasza_linia_produkcyjna.Nazwa_linii}")
+
+@receiver([post_save],sender=LiniaProdukcyjna)
+def zapisz_linie_produkcyjna_przed(sender,instance,**kwargs):
+    print("Właśnie zapisaliśmy: ") 
+    print(f"{instance.Nazwa_linii}")
 class MaszynaProdukcyjna(models.Model):
     Maszyna_nazwa=models.CharField(unique=False,blank=False,
                                    max_length=255,
@@ -41,7 +54,17 @@ class MaszynaProdukcyjna(models.Model):
         verbose_name='Maszyna produkcyjna'
         verbose_name_plural='Maszyny produkcyjne'
         ordering=['Maszyna_nazwa']
-    
+
+# @receiver([pre_save],sender=MaszynaProdukcyjna)
+# def zapisz_maszyne_produkcyjna_przed(sender,instance,**kwargs):
+#     print("Właśnie mamy zapisać maszynę produkcyjną:")
+#     nasza_maszyna_produkcyjna=MaszynaProdukcyjna.objects.get(id=instance.id)
+#     print(f"{nasza_maszyna_produkcyjna.Maszyna_nazwa}")
+
+@receiver([post_save],sender=MaszynaProdukcyjna)
+def zapisz_maszyne_produkcyjna_po(sender,instance,**kwargs):
+    print("Właśnie zapisaliśmy maszynę produkcyjną:")
+    print(f"{instance.Maszyna_nazwa}")
 class UrzadzenieMaszyny(models.Model):
     Nazwa_urzadzenia=models.CharField(unique=False,blank=False,null=False
                                       ,max_length=255
@@ -69,3 +92,14 @@ class UrzadzenieMaszyny(models.Model):
         verbose_name='Urządzenie w maszynie'
         verbose_name_plural='Urządzenia w maszynie'
         ordering=['Ip_Adres']
+
+# @receiver([pre_save],sender=UrzadzenieMaszyny)
+# def zapisz_urzadzenie_maszyny_produkcyjnej_przed(sender,instance,**kwargs):
+#     print("Właśnie mamy zapisać urządzenie maszyny produkcyjnej:")
+#     nasze_urzadzenie_maszyny=UrzadzenieMaszyny.objects.get(id=instance.id)
+#     print(f"{nasze_urzadzenie_maszyny.Nazwa_urzadzenia}")
+
+@receiver([post_save],sender=UrzadzenieMaszyny)
+def zapisz_urzadzenie_maszyny_produkcyjnej_po(sender,instance,**kwargs):
+    print("Właśnie zapisaliśmy urządzenie maszyny produkcyjnej:")
+    print(f"{instance.Nazwa_urzadzenia}")
